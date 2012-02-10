6fe7355ef6bca4eec44a1e3fd7677444304e67e8
Snow Hellsing <snow.hellsing@gmail.com>
2012-02-10 17:07:59 +0800

    mf

--- hypo/urls.py
+++ hypo/urls.py
@@ -15,14 +15,14 @@ urlpatterns = patterns('',
     #url(r'^post/create/$', login_required(v.PostCreateV.as_view())),
     #url(r'^post/create\.(?P<format>html|json)$', login_required(v.PostCreateV.as_view())),
     
-    url(r'^site/(?P<site_slug>[^/]+)/$', include('hypo.post.urls')),
-    url(r'^site/(?P<site_slug>[^/]+)/posts/', include('hypo.post.urls')),
+    url(r'^site/(?P<site_slug>[-\w]+)/$', include('hypo.post.urls')),
+    url(r'^site/(?P<site_slug>[-\w]+)/posts/', include('hypo.post.urls')),
     url(r'^posts/', include('hypo.post.urls')),
     
 #    url(r'^site/(?P<site_slug>[^/]+)/image_sets/', include('hypo.imgset.urls')),
 #    url(r'^image_sets/', include('hypo.imgset.urls')),
     
-    url(r'^site/(?P<site_slug>[^/]+)/images/', include('hypo.img.urls')),
+    url(r'^site/(?P<site_slug>[-\w]+)/images/', include('hypo.img.urls')),
     url(r'^images/', include('hypo.img.urls')),
     #url(r'^album/', include('hypo.album.urls')),
     
