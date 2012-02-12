ca7ac979ea37b0b946af1d3a3be496703f16a50f
Snow Hellsing <snow.hellsing@gmail.com>
2012-02-12 16:18:55 +0800

    pagination

--- hypo/post/views.py
+++ hypo/post/views.py
@@ -11,6 +11,7 @@ class StreamV(gv.ListView):
     model = hypo.Post
     template_name = 'hypo/pg/post_list.html'
     context_object_name = 'post_list'
+    paginate_by = 9
     
     def get_queryset(self):
         site = hypo.Site.from_request(self.request)        
