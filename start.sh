b957ad93c7c8f6c9978b2fdb6acbe08fe8d1afd2
Snow Hellsing <snow.hellsing@gmail.com>
2012-02-15 15:50:49 +0800

    add celeryd logfile path to start.sh

--- start.sh
+++ start.sh
@@ -10,4 +10,5 @@ PIDFILE="$PROJDIR/logs/fcgi.pid"
 sudo ./manage.py runfcgi pidfile=$PIDFILE host=127.0.0.1 port=3051 #daemonize=false
 
 CELERY_PIDFILE="$PROJDIR/logs/celeryd.pid"
-sudo ./manage.py celeryd --pidfile=$CELERY_PIDFILE &
+CELERY_LOGFILE="$PROJDIR/logs/celeryd.log"
+sudo ./manage.py celeryd --pidfile=$CELERY_PIDFILE -f $CELERY_LOGFILE &
